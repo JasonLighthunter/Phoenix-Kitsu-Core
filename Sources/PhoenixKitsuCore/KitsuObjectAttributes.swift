a509bc5427ec1cd2fe14e9d07428d45a84bbd239
Job Cuppen <jobcuppen@live.nl>
2017-12-15 11:36:51 +0100

    added kitsuObjectwithcreatedat and kitsuobjectwithUpdatedAt protocols

--- Sources/PhoenixKitsuCore/KitsuObjectAttributes.swift
+++ Sources/PhoenixKitsuCore/KitsuObjectAttributes.swift
@@ -3,7 +3,15 @@ public protocol KitsuObjectAttributes: Decodable {
   var updatedAt: String? {get}
 }
 
-public protocol KitsuObjectAttributesWithTimestamp: Decodable {
-  var createdAt: String {get}
+public protocol KitsuObjectAttributesWithUpdatedAt: Decodable {
   var updatedAt: String {get}
+}
+
+public protocol KitsuObjectAttributesWithCreatedAt: Decodable {
+  var createdAt: String {get}
+}
+
+public protocol KitsuObjectAttributesWithTimestamp :
+KitsuObjectAttributesWithUpdatedAt, KitsuObjectAttributesWithCreatedAt {
+  
 }
