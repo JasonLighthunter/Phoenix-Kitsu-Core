0dd80a9d47ed47dfd4fc5e0460fa4ca1622780a9
Job Cuppen <jobcuppen@live.nl>
2018-01-10 22:05:04 +0100

    added PhoenixCore

---
+++ Sources/PhoenixKitsuCore/PhoenixCore.swift
@@ -0,0 +1,38 @@
+import Foundation
+import Requestable
+
+public class PhoenixCore {
+  /// Retrieves a (Requestable & Decodable) that corresponds with the given id and type, and
+  /// feeds it to the given clojure
+  ///
+  /// - Parameters:
+  ///   - objectId: The id for the desired object
+  ///   - callback: The callback to be triggered when the object is fetched
+  public class func getResource<T: Requestable & Decodable>(byId objectId: Int,
+                                                            callback: @escaping (T?) -> ()) throws {
+    try KitsuHandler.getResource(by: objectId) { object in callback(object) }
+  }
+
+  /// Retrieves a list of (Requestable & Decodable)s that corresponds with the given filters and
+  /// type, and feeds it to the given clojure
+  ///
+  /// - Parameters:
+  ///   - filters: The filter dictionary to use for searching for the desired objects
+  ///   - callback: The callback to be triggered when the list of objects is fetched
+  
+  public class func getCollection<T>(withFilters filters: [String : String]?,
+                                     callback: @escaping (SearchResult<T>?) -> ()) throws {
+    try KitsuHandler.getCollection(by: filters) { objectList in callback(objectList) }
+  }
+
+  /// Retrieves a list of (Requestable & Decodable)s that corresponds with the given url and
+  /// feeds it to the given clojure
+  ///
+  /// - Parameters:
+  ///   - urlString: The url to use for retrieving a list of objects
+  ///   - callback: The callback to be triggered when the list of objects is fetched
+  public class func getCollection<T>(byURL url: String,
+                                     callback: @escaping (SearchResult<T>?) -> ()) throws {
+    KitsuHandler.getCollection(by: url) { objectList in callback(objectList)}
+  }
+}
