8709a5fad48bb89dd863293592b03a1e3ea27e7c
Job Cuppen <jobcuppen@live.nl>
2018-01-10 22:18:25 +0100

    added networkingUtil and Constants

---
+++ Sources/PhoenixKitsuCore/NetworkingUtil.swift
@@ -0,0 +1,18 @@
+import Foundation
+import Alamofire
+
+class NetworkingUtil {  
+  /// Retrieves json from the kitsu.io API and feeds it to the given clojure
+  ///
+  /// - Parameters:
+  ///   - url: The url of the desired resource or collection
+  ///   - callback: The callback to be triggered when json is fetched
+  class func getDataFrom(_ url: String, callback: @escaping (Data?, Error?) -> Void) {
+    Alamofire.request(url, headers: Constants.baseHeaders).responseData { res in
+      switch res.result {
+      case .failure(let error): callback(nil, error)
+      case .success: callback(res.data, nil)
+      }
+    }
+  }
+}
