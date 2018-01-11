34dde99c74ae41c4f4257d86ce23d8f35915668e
Job Cuppen <jobcuppen@live.nl>
2018-01-11 17:13:42 +0100

    removed commented code

--- Sources/PhoenixKitsuCore/KitsuObject.swift
+++ Sources/PhoenixKitsuCore/KitsuObject.swift
@@ -1,20 +1,3 @@
-//import Requestable
-
-//public protocol KitsuObject: Decodable {
-//  associatedtype KitsuObjectAttributesType: KitsuObjectAttributes
-//
-//  var objectID: String {get}
-//  var type: String {get}
-//  var links: Links {get}
-//  var attributes: KitsuObjectAttributesType? {get}
-//}
-
-//public protocol HasAttributes: Decodable {
-//  associatedtype KitsuObjectAttributesType: KitsuObjectAttributes
-//
-//  var attributes: KitsuObjectAttributesType? {get}
-//}
-
 open class KitsuObject<T: KitsuObjectAttributes>: Decodable {
   public let objectID: String
   public let type: String
