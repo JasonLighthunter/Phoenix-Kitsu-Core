08bf175fb575da979cdc20776189e53cf4548cc4
Job Cuppen <jobcuppen@live.nl>
2018-01-11 16:56:53 +0100

    reworked KitsuObject again

--- Sources/PhoenixKitsuCore/KitsuObject.swift
+++ Sources/PhoenixKitsuCore/KitsuObject.swift
@@ -9,20 +9,30 @@
 //  var attributes: KitsuObjectAttributesType? {get}
 //}
 
-public protocol HasAttributes: Decodable {
-  associatedtype KitsuObjectAttributesType: KitsuObjectAttributes
-  
-  var attributes: KitsuObjectAttributesType? {get}
-}
+//public protocol HasAttributes: Decodable {
+//  associatedtype KitsuObjectAttributesType: KitsuObjectAttributes
+//
+//  var attributes: KitsuObjectAttributesType? {get}
+//}
 
-open class KitsuObject: Decodable {
+open class KitsuObject<T: KitsuObjectAttributes>: Decodable {
   public let objectID: String
   public let type: String
   public let links: Links
+  public let attributes: T?
   
   private enum CodingKeys: String, CodingKey {
     case objectID = "id"
     case type
     case links
+    case attributes
+  }
+  
+  public required init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+    objectID = try container.decode(String.self, forKey: .objectID)
+    type = try container.decode(String.self, forKey: .type)
+    links = try container.decode(Links.self, forKey: .links)
+    attributes = try? container.decode(T.self, forKey: .attributes)
   }
 }
